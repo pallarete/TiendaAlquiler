ed8784d6a964c8d277155761dda2fb8e5c4f695b
avazquez <pallarete@gmail.com>
2024-11-28 15:43:40 +0100

    Eliminar archivos de imágenes del seguimiento de Git

--- TiendaAlquiler/Models/Usuario.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-
-namespace TiendaAlquiler.Models;
-
-public partial class Usuario : IdentityUser
-{
-    public virtual ICollection<Alquiler> Alquilers { get; set; } = new List<Alquiler>();
-}
