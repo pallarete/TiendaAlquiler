d5747cdd53278907f142e43be065e83d8781c134
avazquez <pallarete@gmail.com>
2024-12-09 13:34:01 +0100

    q

---
+++ TiendaAlquiler/Migrations/20241209120511_ColorUpgrade.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TiendaAlquiler.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColorUpgrade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
